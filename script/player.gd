a8dc1be079bcb150a8037f16f9c816f2d0e528a1
mmkanta <47110972+mmkanta@users.noreply.github.com>
2025-10-18 17:57:29 +0700

    Added More things

--- script/player.gd
+++ script/player.gd
@@ -4,19 +4,34 @@ extends CharacterBody2D
 @export var JMP = -400
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 func _physics_process(delta: float):
 	if not is_on_floor():
 		velocity.y += gravity*delta
 	if Input.is_action_just_pressed("space") and is_on_floor():
 		velocity.y = JMP
-		
-	
 	var direction =Input.get_axis("a","d")
 	if direction:
 		velocity.x = direction* SPEED
 	else:
 		velocity.x=move_toward(velocity.x,0,SPEED)
-	
 	move_and_slide()
 	
+
+func _on_room_detector_area_entered(area: Area2D):
+	var collision_shape = area.get_node("CollisionShape2D")
+	var size = collision_shape.shape.extents*2
+	
+	var view_size = get_viewport_rect().size
+	if size.y < view_size.y:
+		size.y = view_size.y
+		
+	if size.x < view_size.x:
+		size.x = view_size.x
+	
+	var cam = $Camera2D
+	cam.limit_top = collision_shape.global_position.y - size.y/2
+	cam.limit_left = collision_shape.global_position.x - size.x/2
+	
+	cam.limit_bottom = cam.limit_top + size.y
+	cam.limit_right = cam.limit_left + size.x
+#
